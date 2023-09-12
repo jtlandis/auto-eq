783c535cbb180df5015e10a7c2a607dec6af05e3
jtlandis <jtlandis314@gmail.com>
2023-09-12 11:13:38 -0400

    simple lua filter auto labels equations

--- _extensions/auto-eq/auto-eq.lua
+++ _extensions/auto-eq/auto-eq.lua
@@ -5,7 +5,40 @@ function Header(el)
   return el
 end
 
+function isDisplayMath(el)
+  return el.t == "Math" and el.mathtype == "DisplayMath"
+end
 
-function Para(eq)
-  print(eq)
+
+function hasEq(el)
+  
+  for i, e in ipairs(el) do
+    if e.t == "Str" then
+      return true
+    end
+  end
+
+  return false
+  
+end
+    
+unnameCounter = 0
+-- adapting from https://github.com/quarto-dev/quarto-cli/blob/56da834f07f5fdfab1e432f11aa3be6b26f4fd2a/src/resources/filters/crossref/equations.lua
+function Para(element)
+  print(element)
+  
+  local inlines = element.content
+  
+  if inlines:find_if(isDisplayMath) == nil then
+    return element
+  end
+  
+  if not hasEq(inlines) then
+    --inlines:insert(pandoc.Space)
+    inlines:insert(pandoc.Str("{#eq-unnamed-" .. unnameCounter .. "}"))
+    --print(element)
+    unnameCounter = unnameCounter + 1
+  end
+  
+  return element
 end
